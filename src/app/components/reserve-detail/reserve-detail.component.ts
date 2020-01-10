3196273bee4d51391de54bf9a2b6626b2d999b07
Lucho <lucianoyabra@gmail.com>
2020-01-10 10:42:27 +0100

    funca

--- src/app/components/reserve-detail/reserve-detail.component.ts
+++ src/app/components/reserve-detail/reserve-detail.component.ts
@@ -86,7 +86,7 @@ export class ReserveDetailComponent implements OnInit {
         // tslint:disable-next-line: max-line-length
         const searchDate = ((new Date(this.actualDay).getUTCFullYear().toString() + '-' + (new Date(this.actualDay).getUTCMonth() +1) + '-'+ new Date(this.actualDay).getUTCDate() + 'T00:00:00.000Z').toString());
         // tslint:disable-next-line: max-line-length
-        alert(searchDate); // ((new Date(Date.now()).getUTCFullYear().toString() + '-' + (new Date(Date.now()).getUTCMonth() +1) + '-'+ new Date(Date.now()).getUTCDate() + 'T00:00:00.000Z').toString())
+        // alert(searchDate); // ((new Date(Date.now()).getUTCFullYear().toString() + '-' + (new Date(Date.now()).getUTCMonth() +1) + '-'+ new Date(Date.now()).getUTCDate() + 'T00:00:00.000Z').toString())
         this._reserveService.getReservesDate(this.token, this.actualDay).subscribe(
           response => {
             if (!response['reserves']) {
